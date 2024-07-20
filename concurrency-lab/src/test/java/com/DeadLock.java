7b43bd2e9b6554bc7eb7be9e2304e057ebc1b5d1
huanruiz <huanruiz@foxmail.com>
2024-07-20 23:47:53 +0800

    dead lock demo

---
+++ concurrency-lab/src/test/java/com/DeadLock.java
@@ -0,0 +1,43 @@
+package com;
+
+/**
+ * @author huanruiz
+ * @since 2024/7/20
+ */
+public class DeadLock {
+
+    private static Object resource1 = new Object();//资源 1
+    private static Object resource2 = new Object();//资源 2
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println(Thread.currentThread() + "get resource1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "thread-1 waiting");
+                synchronized (resource2) {
+                    System.out.println(Thread.currentThread() + "get resource2");
+                }
+            }
+        }, "thread-1").start();
+
+        new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println(Thread.currentThread() + "get resource2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "thread-2 waiting");
+                synchronized (resource1) {
+                    System.out.println(Thread.currentThread() + "get resource1");
+                }
+            }
+        }, "thread-2").start();
+    }
+}
