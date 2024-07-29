510cb7df15f40bf9a29486dc56554c95a37b233f
huanruiz <huanruiz@foxmail.com>
2024-07-29 21:53:17 +0800

    sort

--- java-lab/src/test/java/com/xqt/algorithm/SortLab.java
+++ java-lab/src/test/java/com/xqt/algorithm/SortLab.java
@@ -9,10 +9,11 @@ import java.util.Arrays;
 public class SortLab {
 
     public static void main(String[] args) {
-        int[] arr = {1,3,5,7,2,4,6,8};
+        int[] arr = {1, 3, 5, 7, 2, 4, 6, 8};
         int n = arr.length;
         // bubbleSort(arr);
-
+        // selectSort(arr);
+        insertSort(arr);
 
         System.out.println(Arrays.toString(arr));
     }
@@ -26,11 +27,46 @@ public class SortLab {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int swap = arr[j + 1];
+                    int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
-                    arr[j] = swap;
+                    arr[j] = temp;
                 }
             }
+        }
+    }
+
+    /**
+     * 选择排序: 每次循环把大的放到第一个.
+     * time: O(n^2)
+     * space: O(1)
+     */
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int minIndex = i + 1;
+                if (arr[j] < arr[minIndex]) {
+                    int temp = arr[minIndex];
+                    arr[minIndex] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 插入排序: 判断当前标的与之前的比较, 如果小于之前的元素, 则之前的元素整体右移.
+     * time: 最差: O(n^2), 前面插入数组已经有序: O(n)
+     * space: O(1)
+     */
+    public static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int cur = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > cur) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = cur;
         }
     }
 }
---
+++ java-lab/src/test/java/com/xqt/cocurrency/SafeSingleton.java
@@ -0,0 +1,27 @@
+package com.xqt.cocurrency;
+
+/**
+ * 利用volatile写线程安全的单例
+ * @author huanruiz
+ * @since 2024/7/28
+ */
+public class SafeSingleton {
+
+    private volatile static SafeSingleton singleton;
+
+    private SafeSingleton() {
+
+    }
+
+    public static SafeSingleton getInstance() {
+        // 没实例化才需要加锁实例化, 避免重复加锁操作
+        if (singleton == null) {
+            synchronized (SafeSingleton.class) {
+                if (singleton == null) {
+                    singleton = new SafeSingleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
